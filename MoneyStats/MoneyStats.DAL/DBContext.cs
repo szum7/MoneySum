18d549b63ac75f0f7913b182b4dd91825cb8c01f
Aron Szocs <Aron_Szocs@epam.com>
2019-09-26 10:41:09 +0200

    add projects (.net core 3.0)

---
+++ MoneyStats/MoneyStats.DAL/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MoneyStats.DAL
+{
+    public class TableModelExample
+    {
+
+    }
+
+    public class DBContext : DbContext
+    {
+        public DbSet<TableModelExample> TableModelExample { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
+                //optionsBuilder.UseSqlServer(@"Server=.\;Database=Hostable;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=tcp:hostable.database.windows.net,1433;Initial Catalog=Hostable;Persist Security Info=False;User ID=admn;Password=1host2ABLE3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    }
+}

c93691a7635281988853cec3c68fa77f9bdf28f0
Aron Szocs <Aron_Szocs@epam.com>
2019-10-01 15:27:07 +0200

    example sql inserts

--- MoneyStats/Notes/SQL/2_EXAMPLE_TRANSACTION.sql
+++ MoneyStats/Notes/SQL/2_EXAMPLE_TRANSACTION.sql
@@ -1,10 +1,10 @@
-﻿INSERT INTO dbo.[Currency]
-([Name], [State]) VALUES
-(N'HUF', N'T');
+﻿INSERT INTO dbo.[Currency] ([Name], [State]) VALUES (N'HUF', N'T');
+INSERT INTO dbo.[Currency] ([Name], [STATE]) VALUES (N'EUR', N'T');
 
-INSERT INTO dbo.[Currency]
-([Name], [STATE]) VALUES
-(N'EUR', N'T');
+INSERT INTO dbo.[Tag] ([Title], [STATE]) VALUES (N'Bank', N'T');
+INSERT INTO dbo.[Tag] ([Title], [STATE]) VALUES (N'Csomagdíj', N'T');
+INSERT INTO dbo.[Tag] ([Title], [STATE]) VALUES (N'Készpénzfelvét', N'T');
+INSERT INTO dbo.[Tag] ([Title], [STATE]) VALUES (N'Élelmiszer', N'T');
 
 INSERT INTO dbo.[Transaction] 
 ([AccountingDate], [TransactionId], [Type], [Account], [AccountName], [PartnerAccount], [PartnerName], [Sum], [CurrencyId], [Message], [State]) VALUES
@@ -13,3 +13,8 @@ INSERT INTO dbo.[Transaction]
 INSERT INTO dbo.[Transaction] 
 ([AccountingDate], [TransactionId], [Type], [Account], [AccountName], [PartnerAccount], [PartnerName], [Sum], [CurrencyId], [Message], [State]) VALUES
 ('2016-05-27', N'440602******4503', N'Készpénzfelvét K&H ATM-ből', N'104040657157575649481012', N'SZŐCS ÁRON', NULL, N'SZENTMIHALYI UT 131.', -20000.00, 1, NULL, N'T');
+
+INSERT INTO dbo.[TransactionTagConn] ([TransactionId], [TagId]) VALUES (1, 1);
+INSERT INTO dbo.[TransactionTagConn] ([TransactionId], [TagId]) VALUES (1, 2);
+INSERT INTO dbo.[TransactionTagConn] ([TransactionId], [TagId]) VALUES (2, 1);
+INSERT INTO dbo.[TransactionTagConn] ([TransactionId], [TagId]) VALUES (2, 3);

0d1cfc4ca7f2d3c12191c5d99038d4613d14a3f4
Aron Szocs <Aron_Szocs@epam.com>
2019-10-07 10:52:36 +0200

    work with ProgramRunner in Program

--- MoneyStats/ExcelWorkerApp/Program.cs
+++ MoneyStats/ExcelWorkerApp/Program.cs
@@ -12,33 +12,17 @@ namespace ExcelWorkerApp
     {
         static void Main(string[] args)
         {
-            var excelReader = new ExcelReader<ExcelTransaction>();
-            var excelWriter = new ExcelWriter();
-            var transactionRepo = new TransactionRepository();
-            var mergedFileReader = new ExcelReader<ExcelTransactionExtended>();
-            var transactionMerger = new TransactionMerger();
-
-
             // Test section
 #if false
             var list = (new MergeTest()).MergeTestMethod();
             return;
 #endif
 
-            // Read many bank-exported excel files
-#if true
-            ExcelSheet<ExcelTransaction> allFiles = excelReader.Read(@"C:\Users\Aron_Szocs\Documents\Bank", "*.xls");
-#endif
+            var run = new ProgramRunner();
 
-            // Merge read files
-#if true
-            excelReader.TruncateData(allFiles);
-#endif
-
-            // Write merged file to an excel file
-#if true
-            excelWriter.Run(allFiles, @"C:\Users\Aron_Szocs\Documents\Bank\Merged\Merged.xls");
-#endif
+            run.ReadManyBankExportedFiles(@"C:\Users\Aron_Szocs\Documents\Bank", "*.xls");
+            run.TruncateBankExportedFiles();
+            run.CreateMergedExcelFile(@"C:\Users\Aron_Szocs\Documents\Bank\Merged\Merged.xls");
 
             // => User edits the file
 #if true
@@ -58,28 +42,10 @@ namespace ExcelWorkerApp
             }
 #endif
 
-            // Read one merged excel file
-#if true
-            mergedFileReader.IsReadFromTheBeginning = true;
-            var mergedFile = mergedFileReader.Read(@"C:\Users\Aron_Szocs\Documents\Bank\Merged\Merged.xls");
-#endif
-
-            // Load already existing transactions from database
-#if true
-            var transactionsFromDB = transactionRepo.GetWithEntities();
-#endif
-
-            // Merge db data and merged-excel file data
-#if true
-            var mergedList = transactionMerger.Run(transactionsFromDB, mergedFile.Transactions);
-            excelWriter.Run(mergedList, @"C:\Users\Aron_Szocs\Documents\Bank\Merged\MergedList.xls");
-            var unsavedTransactionList = transactionMerger.GetNewRows(mergedList);
-#endif
-
-            // Write to db
-#if true
-            transactionRepo.SmartSave(unsavedTransactionList);
-#endif
+            run.ReadExtendedTransactionsMergedFile(@"C:\Users\Aron_Szocs\Documents\Bank\Merged\Merged.xls");
+            run.LoadAlreadyExistingTransactionsFromDatabase();
+            run.MergeDatabaseAndExtendedMergedTransactions();
+            run.SaveNewBankExportedTransactions();
 
             Console.WriteLine("PROGRAM ENDED.");
         }

84d7acd3add0fade3425806682077d052c24acdd
Aron Szocs <Aron_Szocs@epam.com>
2019-10-10 13:45:46 +0200

    use basic angular skeleton

--- MoneyStats/MoneyStats.WebApp/ClientApp/src/app/app.module.ts
+++ MoneyStats/MoneyStats.WebApp/ClientApp/src/app/app.module.ts
@@ -1,34 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faInfoCircle, faCaretRight, faCaretLeft, faSun, faCog, faTimes } from '@fortawesome/free-solid-svg-icons';
 
+// Components
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
+
+// Pages
+import { HomePage } from './pages/home-page/home.page';
+
+// Services
+import { LoadingScreenService } from './services/loading-screen-service/loading-screen.service';
+import { RouterService } from './services/router-service/router.service';
 
 @NgModule({
   declarations: [
+    // Components
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    NavComponent,
+    LoadingScreenComponent,
+    // Pages
+    HomePage
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
+    BrowserModule,
+    FontAwesomeModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: '**', component: HomePage }
+    ], { useHash: true })
   ],
-  providers: [],
+  providers: [
+    LoadingScreenService,
+    RouterService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    library.add(faInfoCircle);
+  }
+}

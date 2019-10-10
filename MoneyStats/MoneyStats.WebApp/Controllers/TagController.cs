ef7fe449845f5e539ea274ef337405f12821fb51
Aron Szocs <Aron_Szocs@epam.com>
2019-10-10 13:23:34 +0200

    add MoneyStats.WebApp

---
+++ MoneyStats/MoneyStats.WebApp/Controllers/TagController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyStats.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyStats.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagController : ControllerBase
+    {
+        [HttpGet("testcall")]
+        public ActionResult TestCall()
+        {
+            return Ok(123);
+        }
+
+        [HttpGet("gettags")]
+        public ActionResult GetTags()
+        {
+            var repo = new TagRepository();
+            return Ok(repo.Get());
+        }
+    }
+}

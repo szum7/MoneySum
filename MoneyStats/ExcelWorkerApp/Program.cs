6947104d366c7dc6e7088cbc4dfd6fec623df8ba
Szőcs Áron <szocs.aa@gmail.com>
2019-12-28 19:03:32 +0100

    program

--- MoneyStats/ExcelWorkerApp/Program.cs
+++ MoneyStats/ExcelWorkerApp/Program.cs
@@ -25,9 +25,20 @@ namespace ExcelWorkerApp
             var run = new ProgramRunner();
             var now = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
             var nowString = "2019-11-03-8-52-36";
+            const string bankFilesPath = @"C:\Users\Shy\Documents\Ego\AllDocs\bank";
+            const string mergedFilePath = @"C:\Users\Shy\Documents\Ego\AllDocs\bank\Merged\Merged_";
 
             Console.WriteLine("" +
-                "[1] CLEAR AND REBUILD: (unavailable)\n" +
+                "[1] CREATE MERGED FILE:\n" +
+                "1. Read bank files.\n" +
+                "2. Create new merged file.\n\n" +
+
+                "[2] READ MERGED FILE AND REBUILD:\n" +
+                "4. Read (edited) merged file.\n" +
+                "5. Clear database. \n" +
+                "6. Write to database.\n\n" +
+
+                "[3] CLEAR AND REBUILD:\n" +
                 "1. Read bank files.\n" +
                 "2. Create new merged file.\n" +
                 "3. Await user to edit the merged file.\n" +
@@ -35,7 +46,7 @@ namespace ExcelWorkerApp
                 "5. Clear database. \n" +
                 "6. Write to database.\n\n" +
 
-                "[2] ADD TO DATABASE: (unavailable)\n" +
+                "[4] ADD TO DATABASE: (unavailable)\n" +
                 "1. Read bank files.\n" +
                 "2. Add not-yet-existing transactions to a new merged file.\n" +
                 "3. Await user to edit the merged file.\n" +
@@ -48,20 +59,30 @@ namespace ExcelWorkerApp
             switch (command.ToLower())
             {
                 case "1":
-                    run.ReadManyBankExportedFiles(@"C:\Users\Shy\Documents\Ego\AllDocs\bank", "*.xls");
+                    run.ReadManyBankExportedFiles(bankFilesPath, "*.xls");
                     run.TruncateBankExportedFiles();
-                    run.CreateMergedExcelFile($@"C:\Users\Shy\Documents\Ego\AllDocs\bank\Merged\Merged_{now}.xls");
+                    run.CreateMergedExcelFile($"{mergedFilePath}{now}.xls"); 
+                    break;
+                case "2":                    
+                    run.ReadExtendedTransactionsMergedFile($"{mergedFilePath}{now}.xls");
+                    run.ClearTransactionRelatedDataFromDatabase();
+                    run.SaveBankExportedTransactions();
+                    break;
+                case "3":
+                    run.ReadManyBankExportedFiles(bankFilesPath, "*.xls");
+                    run.TruncateBankExportedFiles();
+                    run.CreateMergedExcelFile($"{mergedFilePath}{now}.xls");
                     AwaitToEditMergedFileProgram("" +
-                        "1. [Done] Bank files read.\n" +
-                        "2. [Done] Merged file created.\n" +
+                        "[Done] 1. Bank files read.\n" +
+                        "[Done] 2. Merged file created.\n" +
                         "3. Awaiting user to edit the merged file.\n" +
                         "Edit the merged file and type OK to progress. Or type EXIT to exit.");
-                    run.ReadExtendedTransactionsMergedFile($@"C:\Users\Shy\Documents\Ego\AllDocs\bank\Merged\Merged_{now}.xls");
-                    run.ClearTransactionRelatedDataFromDatabase();                    
+                    run.ReadExtendedTransactionsMergedFile($"{mergedFilePath}{now}.xls");
+                    run.ClearTransactionRelatedDataFromDatabase();
                     run.SaveBankExportedTransactions();
-
                     break;
-                case "2":
+                case "4":
+                    Console.WriteLine("Not yet implemented!");
                     break;
                 case "exit":
                     Console.WriteLine("Program was exited.");
@@ -72,13 +93,13 @@ namespace ExcelWorkerApp
             }
 
 
-            // Test section
-#if true
+            #region Test section
+#if false
             var list = (new TransactionRepository()).Get();
             return;
 #endif
+            #endregion
 
-            
 
 #if false
             run.ReadManyBankExportedFiles(@"C:\Users\Shy\Documents\Ego\AllDocs\bank", "*.xls");
@@ -104,7 +125,7 @@ namespace ExcelWorkerApp
             }
 #endif
 
-#if true
+#if false
             run.ReadExtendedTransactionsMergedFile($@"C:\Users\Shy\Documents\Ego\AllDocs\bank\Merged\Merged_{nowString}.xls");
             run.LoadAlreadyExistingTransactionsFromDatabase();
             run.MergeDatabaseAndExtendedMergedTransactions();

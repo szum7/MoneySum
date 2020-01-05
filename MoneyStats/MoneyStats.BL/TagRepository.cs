347194f0e8a9c2db82e1c42f9649312dfd1cc6a4
Szőcs Áron <szocs.aa@gmail.com>
2020-01-05 23:53:11 +0100

    GetAllTagDetailedSummary (unfinished)

--- MoneyStats/MoneyStats.BL/TagRepository.cs
+++ MoneyStats/MoneyStats.BL/TagRepository.cs
@@ -52,15 +52,54 @@ namespace MoneyStats.BL
             }
         }
 
+        class TagMonthSummary
+        {
+            public DateTime Month { get; set; }
+            public string Title { get; set; }
+            public decimal Income { get; set; }
+            public decimal Expense { get; set; }
+            public decimal Flow { get { return Income - Math.Abs(Expense); } }
+            public List<Transaction> Transactions { get; set; }
+        }
+
         public void GetAllTagDetailedSummary(int tagId, DateTime from, DateTime to)
         {
-            var dates = Common.GetDatesList(from, to);
+            var months = Common.GetMonthsList(from, to);
+            var monthsDict = new Dictionary<DateTime, TagMonthSummary>();
             var transRepo = new TransactionRepository();
             var transTagConnRepo = new TransactionTagConnRepository();
             var tagRepo = new TagRepository();
 
+            var ttcs = transTagConnRepo.GetWithEntities();
+
+            foreach (var ttc in ttcs)
+            {
+                var current = ttc.Transaction;
+                var currentMonth = new DateTime(current.AccountingDate.Year, current.AccountingDate.Month, 1);
+                if (!monthsDict.ContainsKey(currentMonth))
+                {
+                    monthsDict.Add(currentMonth, new TagMonthSummary()
+                    {
+                        Month = currentMonth,
+                        Title = ttc.Tag.Title,
+                        Income = (current.Sum > 0 ? current.Sum.Value : 0),
+                        Expense = (current.Sum < 0 ? current.Sum.Value : 0),
+                        Transactions = new List<Transaction>() { current }
+                    });
+                } 
+                else
+                {
+                    var monthDictRow = monthsDict[currentMonth];
+                    monthDictRow.Income += (current.Sum > 0 ? current.Sum.Value : 0);
+                    monthDictRow.Expense += (current.Sum < 0 ? current.Sum.Value : 0);
+                    monthDictRow.Transactions.Add(current);
+                    // TODO ROSSZ!!! nem 1 tag van hónaponként! kijjebb vinni a dátumot!
+                }
+            }
+
             // create year-month dictionary
             // iterate transTagConnList and associate on date <-> year-month key
+            // orderby date
         }
 
         public void DeleteAll()
--- MoneyStats/MoneyStats.BL/Utility/Common.cs
+++ MoneyStats/MoneyStats.BL/Utility/Common.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace MoneyStats.BL.Utility
 {
     public class Common
     {
-        public static List<DateTime> GetDatesList(DateTime from, DateTime to)
+        // TODO test if from and to is in the array + test if all x.01
+        public static List<DateTime> GetMonthsList(DateTime from, DateTime to)
         {
-            return Enumerable
-                .Range(0, 1 + to.Subtract(from).Days)
-                .Select(offset => from.AddDays(offset))
-                .ToList();
+            var months = new List<DateTime>();
+
+            for (var dt = from; dt <= to; dt = dt.AddMonths(1))
+            {
+                months.Add(dt);
+            }
+
+            return months;
         }
     }
 }
